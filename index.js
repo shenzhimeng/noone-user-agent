7e3be14cde7328c39c31fbd2d0b82e62c1d50774
0xBeWater <shenzhimeng@users.noreply.github.com>
2024-03-09 02:15:25 +0800

    Newest

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Newest
 var UA = [
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:123.0) Gecko/20100101 Firefox/123.0",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
